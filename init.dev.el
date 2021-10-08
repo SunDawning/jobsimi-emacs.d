b7298bf212052a98f2b239d80b91fadd402ff220
sunshg <sunshg@szsxtc.com>
2021-10-08 11:02:07 +0800

    新增 可以使用tuna源来安装包

--- init.dev.el
+++ init.dev.el
@@ -4,3 +4,7 @@
   )
 (with-eval-after-load (quote js)
   (add-hook (quote js-mode-hook) (function jobsimi::js-mode-hook)))
+(setf package-archives
+      (quote
+       (("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+	("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/"))))
